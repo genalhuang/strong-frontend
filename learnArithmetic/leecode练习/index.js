de37c805908c80dd2e24e0c1364b2e5fd362919c
genaller <3505563308@qq.com>
2020-06-08 21:44:52 +0800

    什么都没学到

--- learnArithmetic/leecode练习/index.js
+++ learnArithmetic/leecode练习/index.js
@@ -1,106 +1,27 @@
-/**
- * @param {number} num
- * @return {string}
- */
-var intToRoman = function (num) {
-  let s = num.toString()
-  let sum = ''
-  let arr = [1,10,100,1000,10000,100000]
-  for(var i=0;i<s.length;i++) {
-    if(s[i] != 0) {
-      sum += zidian(s[i] * arr[s.length-1-i])
-    }
 
-  }
-  return sum
-};
-function zidian(num) {
-  if (1 <= num && num < 10) {
-    if (num === 4 || num === 9) {
-      if (num === 4) {
-        return 'IV'
-      } else {
-        return 'IX'
-      }
-    }
-    let s = ''
-    if (num >= 5) {
-      s = 'V'
-      for (var i = 0; i < num - 5; i += 1) {
-        s += 'I'
-      }
-      return s
-    } else {
-      for (var i = 0; i < num; i += 1) {
-        s += 'I'
-      }
-      return s
-    }
-  }
-  if (10 <= num && num < 100) {
-    if (num === 40 || num === 90) {
-      if (num === 40) {
-        return 'XL'
-      } else {
-        return 'XC'
-      }
-    }
-    let s = ''
-    if (num >= 50) {
-      s = 'L'
-      for (var i = 0; i < num - 50; i += 10) {
-        s += 'X'
-      }
-      return s
-    } else {
-      for (var i = 0; i < num; i += 10) {
-        s += 'X'
-      }
-      return s
-    }
-  }
-  if (100 <= num && num < 1000) {
-    if (num === 400 || num === 900) {
-      if (num === 400) {
-        return 'CD'
-      } else {
-        return 'CM'
-      }
-    }
-    let s = ''
-    if (num >= 500) {
-      s = 'D'
-      for (var i = 0; i < num - 500; i += 100) {
-        s += 'C'
-      }
-      return s
-    } else {
-      for (var i = 0; i < num; i += 100) {
-        s += 'C'
-      }
-      return s
-    }
+var letterCombinations = function(ms) {
+  const H = '时';
+  const M = '分';
+  const S = '秒';
+  let hour = parseInt((ms % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let standardHour = hour + 8;
+  if (standardHour > 24) hour = hour % 24;
+  let minutes = parseInt((ms % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = parseInt((ms % (1000 * 60)) / 1000);
 
-  }
-  if (1000 <= num && num < 1000000) {
-    let s = ''
-    for (var i = 0; i < num; i += 1000) {
-      s += 'M'
-    }
-    return s
-  }
-}
+  let min = minutes < 10 ? `0${minutes}` : minutes;
+  let h = hour < 10 ? `0${hour}` : hour;
+  let s = seconds < 10 ? `0${seconds}` : seconds;
 
-console.log(intToRoman(5000))
-
-
-// 真实玩法
-var intToRoman = function(num) {
-  var Q = ["", "M", "MM", "MMM"];
-  var B = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
-  var S = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
-  var G = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
-  return Q[Math.floor(num/1000)] + B[Math.floor((num%1000)/100)] + S[Math.floor((num%100)/10)] + G[num%10];
+  return {
+    time: `${standardHour}:${min}`,
+    desc: hour > 0 ? `${hour}${H}${minutes}${M}${seconds}${S}` : `${minutes}${M}${seconds}${S}`,
+    desc2: hour > 0 ? `${h}:${min}:${s}` : `${min}:${s}`,
+    hour: hour,
+    minutes: minutes,
+    seconds: seconds,
+  };
+  
 };
 
-console.log(intToRoman(500))
+console.log(letterCombinations(2399792))
