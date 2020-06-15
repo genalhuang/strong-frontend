40ddb3bb2c4d741a23628c7e0f1c1aeef48eccaa
genaller <3505563308@qq.com>
2020-06-15 19:52:04 +0800

    深拷贝进阶

--- learnArithmetic/前端高频面试题/对象深拷贝.js
+++ learnArithmetic/前端高频面试题/对象深拷贝.js
@@ -1,36 +1,45 @@
-// 垃圾简单版
-var data = {
-  val: [1,2,4,5],
-  left:  {
-    val: 2,
-    left: null,
-    rigth: {
-      val: 3
+// 基础版
+// 没有解决set里面包含了引用类型怎么处理
+var obj = {
+  number: 1,
+  string: 'string',
+  array: [1, 2, 3, 4],
+  obj: {
+    obj: {
+      number: 2
     }
   },
-  right:  function () {
-    console.log(a)
-  }
+  regexp: new RegExp(/a/),
+  set: new Set().add([1, 2, 3, 4, 5, 6])
 }
+obj.set.add('string')
+
 
 function deepMerge(data) {
-  if(Object.prototype.toString.call(data) !== '[object Object]') {
+  if (Object.prototype.toString.call(data) !== '[object Object]') {
     throw new Error('无法拷贝非对象类型')
   }
   let mydata = {}
-  for(var key in data) {
-    if(Object.prototype.toString.call(data[key]) === '[object Array]') {
+  for (var key in data) {
+    if (Object.prototype.toString.call(data[key]) === '[object Array]') {
       mydata[key] = []
-      for(var item of data[key]) {
+      for (var item of data[key]) {
         mydata[key].push(item)
       }
-    } else if(Object.prototype.toString.call(data[key]) === '[object Object]') {
+    } else if (Object.prototype.toString.call(data[key]) === '[object Object]') {
       mydata[key] = deepMerge(data[key])
+    } else if (Object.prototype.toString.call(data[key]) === '[object Function]') {
+      mydata[key] = data[key].bind()
+    } else if (Object.prototype.toString.call(data[key]) === '[object Set]') {
+      mydata[key] = new Set()
+      for (var item of data[key]) {
+        mydata[key].add(item)
+      }
     } else {
       mydata[key] = data[key]
     }
   }
   return mydata
 }
-
-console.log(deepMerge(data))
+var b = deepMerge(obj)
+console.log(b, obj)
